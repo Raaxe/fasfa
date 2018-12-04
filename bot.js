efad4595a750663f808ae6982d39ad29b88179bd
ReBel <mrxhm1555@gmail.com>
2018-12-04 16:04:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2414,12 +2414,131 @@ client.on('guildMemberAdd', member=> {
     member.addRole(member.guild.roles.find("name","member"));
     });
 
-client.on('ready', () => {
-    setInterval(function(){
-        client.guilds.get('514973273633587202').roles.find('name', '..').edit({color: 'RANDOM'})
-    },12000);
-  
 
-})
+
+client.on('message', msg => {
+  if (msg.content === 'السعودية') {      
+    msg.react("????")
+    msg.channel.send("????")
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'مصر') {      
+    msg.react("????")
+    msg.channel.send("????")
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'المغرب') {      
+    msg.react("????")
+    msg.channel.send("????")
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'العراق') {      
+    msg.react("????")
+    msg.channel.send("????")
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'الجزائر') {      
+    msg.react("????")
+    msg.channel.send("????")
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'الامارات') {      
+    msg.react("????")
+    msg.channel.send("????")
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'تونس') {      
+    msg.react("????")
+    msg.channel.send("????")
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'سوريا') {      
+    msg.react("????")
+    msg.channel.send("????")
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ليبيا') {      
+    msg.react("????")
+    msg.channel.send("????")
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'قطر') {      
+    msg.react("????")
+    msg.channel.send("????")
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'الصومال') {      
+    msg.react("????")
+    msg.channel.send("????")
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'عمان') {      
+    msg.react("????")
+    msg.channel.send("????")
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'موريتانيا') {      
+    msg.react("????")
+    msg.channel.send("????")
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'فلسطين') {      
+    msg.react("????")
+    msg.channel.send("????")
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'البحرين') {      
+    msg.react("????")
+    msg.channel.send("????")
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'امريكا') {      
+    msg.react("????")
+    msg.channel.send("????")
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'الكعبة') {      
+    msg.react("??")
+    msg.channel.send("??")
+  }
+});
+client.on('message', msg => {
+  if (msg.content === 'اسرائيل') {      
+    msg.react("??")
+    msg.channel.send("الشيت يرجال")
+  }
+});
 
 client.login(process.env.BOT_KEY);
